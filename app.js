5145a104c5474856cbcaef32f28d514b4c3e2b65
Nate Christiansen <natechristiansen42@gmail.com>
2017-06-09 10:58:12 -0400

    fix

--- app.js
+++ app.js
@@ -9,6 +9,7 @@
 var fs = require('fs');
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -21,6 +22,8 @@ var app = express();
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/customers', function(req, res, next) {
   res.send(db);
--- public/addNewDevice.js
+++ public/addNewDevice.js
@@ -2,7 +2,9 @@
 
 function createNewDevice()	{
 	$.get('/customers', function(data){
-		$.post("/savedatabase", {db: data});
+		$.post("/savedatabase", {db: data}, function(res) {
+			console.log(res);
+		}, "json");
 	});
 
 }
